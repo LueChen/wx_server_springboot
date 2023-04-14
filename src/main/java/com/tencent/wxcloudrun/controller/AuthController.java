7b9c0aa66f55ae6e5027cd3a7d466d9410794a3d
Lue Chen <luechen@foxmail.com>
2023-04-14 22:47:49 +0800

    fix illegal writer

--- src/main/java/com/tencent/wxcloudrun/controller/AuthController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.tencent.wxcloudrun.utils.CheckSignatureUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,17 +14,17 @@ import java.io.UnsupportedEncodingException;
 @RestController
 public class AuthController {
     @GetMapping("/wx")
-    public void checkSign(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+    public void checkSign(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding("UTF-8");
         String signature = request.getParameter("signature");
         String timestamp = request.getParameter("timestamp");
         String nonce = request.getParameter("nonce");
         String echostr = request.getParameter("echostr");
-        PrintWriter out = null;
+        ServletOutputStream out = null;
         try {
-            out = response.getWriter();
+            out = response.getOutputStream();
             if (CheckSignatureUtil.checkSignature(signature, timestamp, nonce)) {
-                out.write(echostr);
+                out.write(echostr.getBytes());
             }
         } catch (IOException e) {
             e.printStackTrace();
