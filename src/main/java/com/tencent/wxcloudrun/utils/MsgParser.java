566cb3d5115f64b004ef9d364ddd4bb8f6c1f237
Lue Chen <luechen@foxmail.com>
2023-04-14 22:48:13 +0800

    first commit for signature

---
+++ src/main/java/com/tencent/wxcloudrun/utils/MsgParser.java
@@ -0,0 +1,29 @@
+package com.tencent.wxcloudrun.utils;
+
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MsgParser {
+    public static Map<String, String> parseXml(HttpServletRequest request) throws Exception {
+        Map<String, String> map = new HashMap<String, String>();
+        InputStream inputStream = request.getInputStream();
+        SAXReader reader = new SAXReader();
+        Document document = reader.read(inputStream);
+        Element root = document.getRootElement();
+        List<Element> elementList = root.elements();
+        for (Element e : elementList)
+            map.put(e.getName(), e.getText());
+        inputStream.close();
+        inputStream = null;
+        return map;
+    }
+}
+
